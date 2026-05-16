c6888aa3c3413b4dbb980227b1e4429450fae334
Arjit Shukla <arjitshukla286@gmail.com>
2026-05-16 13:31:09 +0530

    Setup PostgreSQL and SQLAlchemy

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from .core.config import settings
+from .db.database import engine
+from .db.models import Base
 
+
+Base.metadata.create_all(bind=engine)
 app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG)
 
 @app.get("/")
 def read_root():
-    return {"message ": "Oauth Project is working!"}
+    return {"message": "DB Connected"}
